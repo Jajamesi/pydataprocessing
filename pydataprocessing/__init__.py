da0e00e7c3c562ba79784478c5bcc7ae78d1e1e2
Jajamesi <145933909+Jajamesi@users.noreply.github.com>
2024-11-26 13:14:36 +0300

    Update __init__.py
    shutdown streamlit

--- pydataprocessing/__init__.py
+++ pydataprocessing/__init__.py
@@ -15,12 +15,12 @@ from .worker import (read_spss,
                      gen_script_dir
                      )
 
-from .streamlit_custom import (v_space,
-                               file_nullify,
-                               error,
-                               set_pages_layout,
-                               clear_sss
-                               )
+# from .streamlit_custom import (v_space,
+#                                file_nullify,
+#                                error,
+#                                set_pages_layout,
+#                                clear_sss
+#                                )
 
 from .data_processor import (create_table,
                              table_export,
